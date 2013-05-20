c81bf0819e850a68c5a3b57b15e3d727b6a9ebf5
unknown <319143@wks23-014.schools.mybps.org>
2013-05-20 13:26:10 -0400

    First steps to pop up image gallery

---
+++ src/gui/map/ImagePopUp.java
@@ -0,0 +1,37 @@
+import java.awt.Container;
+import java.awt.Insets;
+import javax.swing.JFrame;
+
+
+public class ImagePopUp {
+
+	// Singleton
+	private static final ImagePopUp instance = new ImagePopUp();
+
+	// Components
+	private JFrame popFrame;
+	private Container popPane;
+	private Insets popInsets;
+	
+	// Properties
+	private final int INIT_WIDTH = 200;
+	private final int INIT_HEIGHT = 200;
+
+	// Constructor
+	private ImagePopUp() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			// Do not handle because fanciness is unimportant
+		}
+
+		popFrame = new JFrame("Image Gallery");
+		popFrame.setSize(INIT_WIDTH, INIT_HEIGHT);
+
+		popPane = popFrame.getContentPane();
+		popPane.setLayout(null);
+
+		popInsets = popPane.getInsets();
+	}
+
+}
